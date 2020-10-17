587e349282c4491fb0cbc53b6f155a441a6b25aa
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2020-10-17 12:44:25 +0100

    added display2

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 // create a simple function to replace the innerhtml of the box you want to fill with the form when you click on the action
 
-function show(change) {
+function display1(change) {
     document.getElementById('box2').innerHTML = document.getElementById(change).innerHTML;
+  }
+
+  function display2(change) {
+    document.getElementById('box3').innerHTML = document.getElementById(change).innerHTML;
   }
